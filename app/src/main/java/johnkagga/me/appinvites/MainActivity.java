691c10e28d015a6f1666161743bf4a0a8a7882b1
johnkagga <johnkagga@gmail.com>
2015-12-30 22:29:13 +0300

    Receiving and parsing the Deep Link

--- app/src/main/java/johnkagga/me/appinvites/MainActivity.java
+++ app/src/main/java/johnkagga/me/appinvites/MainActivity.java
@@ -1,7 +1,9 @@
 package johnkagga.me.appinvites;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -9,7 +11,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.appinvite.AppInvite;
 import com.google.android.gms.appinvite.AppInviteInvitation;
+import com.google.android.gms.appinvite.AppInviteInvitationResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private static final CharSequence INVITATION_MSG = "Invite your friends to enjoy the goodness";
     private static final CharSequence INVITATION_CALL_TO_ACTION = "Share";
     private static final int REQUEST_INVITE = 500;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     private Button appButton;
 
@@ -34,10 +42,32 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new AppInviteInvitation.IntentBuilder(INVITATION_TITLE)
                         .setMessage(INVITATION_MSG)
                         .setCallToActionText(INVITATION_CALL_TO_ACTION)
+                        .setDeepLink(Uri.parse("johnkagga://app.invite/50"))
                         .build();
                 startActivityForResult(intent, REQUEST_INVITE);
             }
         });
+
+        //Receiving and parsing the custom Uri
+        GoogleApiClient apiClient = new GoogleApiClient.Builder(this)
+                .addApi(AppInvite.API)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Log.v(LOG_TAG,"Failed connection: " + connectionResult);
+                        showMessage("Connection Failed");
+                    }
+                })
+                .build();
+        //Parsing the invite
+        AppInvite.AppInviteApi.getInvitation(apiClient,this,true)
+                .setResultCallback(new ResultCallback<AppInviteInvitationResult>() {
+                    @Override
+                    public void onResult(@NonNull AppInviteInvitationResult appInviteInvitationResult) {
+                        //true above automatically calls the AppInvited activity
+                    }
+                });
+
     }
 
     /**
