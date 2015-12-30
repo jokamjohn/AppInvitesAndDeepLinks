2a61230821c41886f7bb283fa3ba0156a6363009
johnkagga <johnkagga@gmail.com>
2015-12-30 22:42:08 +0300

    Recieving the app invite and deep link

--- app/src/main/java/johnkagga/me/appinvites/AppInvited.java
+++ app/src/main/java/johnkagga/me/appinvites/AppInvited.java
@@ -1,7 +1,12 @@
 package johnkagga.me.appinvites;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.appinvite.AppInviteReferral;
 
 public class AppInvited extends AppCompatActivity {
 
@@ -10,4 +15,25 @@ public class AppInvited extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_invited);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //check whether the intent contains deep link data
+        Intent intent = getIntent();
+        if (AppInviteReferral.hasReferral(intent))
+        {
+            String deepLink = AppInviteReferral.getDeepLink(intent);
+            Log.d(getString(R.string.app_name),
+                    "Found Referral: " + AppInviteReferral.getInvitationId(intent) + ":" + deepLink);
+
+            String[] array = deepLink.split("/");
+
+            if (array.length > 0) {
+                TextView tv = (TextView) findViewById(R.id.discount);
+                tv.setText(String.format(tv.getText().toString(), array[array.length-1]));
+            }
+        }
+    }
 }
+
